7a1ec8862e5c4abe465ca5868f5ceab0e618d0ba
Unknown <simon.dube@hotmail.com>
2018-07-30 16:56:39 -0400

    stdout and stderr adaptation for Datalad

--- server/resources/helpers/std.py
+++ server/resources/helpers/std.py
@@ -2,11 +2,13 @@ from flask import Response
 from server.database import db
 from server.common.utils import marshal
 from server.common.error_codes_and_messages import (
-    ErrorCodeAndMessageFormatter, EXECUTION_NOT_FOUND, UNAUTHORIZED)
+    ErrorCodeAndMessageFormatter, EXECUTION_NOT_FOUND, UNAUTHORIZED, UNEXPECTED_ERROR)
 from server.database.queries.executions import get_execution
 from server.resources.helpers.executions import (
-    get_execution_as_model, get_std_file, STDERR_FILENAME, is_safe_for_get)
+    get_execution_as_model, get_std_file, STDERR_FILENAME, is_safe_for_get, std_file_path)
 from server.resources.decorators import login_required
+from server.datalad_f.utils import (
+    get_data_dataset, datalad_get)
 
 
 def std_file_resource(user, execution_identifier, path_to_file):
@@ -17,12 +19,21 @@ def std_file_resource(user, execution_identifier, path_to_file):
         return marshal(error), 400
 
     if not is_safe_for_get(user, execution_db):
-        return UNAUTHORIZED
+        return marshal(UNAUTHORIZED), 400
 
     execution, error = get_execution_as_model(execution_db.creator_username,
                                               execution_db)
     if error:
         return marshal(error), 400
+
+    # If using Datalad, first get the file
+    dataset = get_data_dataset()
+    if dataset:
+        success = datalad_get(dataset, std_file_path(
+            user.username, execution_identifier, path_to_file))
+        dataset.close()
+        if not success:
+            return marshal(UNEXPECTED_ERROR), 400
 
     std, error = get_std_file(user.username, execution_identifier,
                               path_to_file)
